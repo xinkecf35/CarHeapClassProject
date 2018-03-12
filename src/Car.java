a1df210cfdffe53f1ade80e388b5e2c6aafa0c4d
Xinke Chen <xinkecf35@gmail.com>
2018-03-12 16:43:48 -0400

    First commit

---
+++ src/Car.java
@@ -0,0 +1,65 @@
+import java.util.Objects;
+
+public class Car {
+    private String VIN;
+    private String make;
+    private String model;
+    private String color;
+    private double price;
+    private int mileage;
+
+
+    public Car(String VIN,
+               String make,
+               String model,
+               String color,
+               double price,
+               int mileage) {
+        this.VIN = VIN;
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.price = price;
+        this.mileage = mileage;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(VIN, car.VIN) &&
+                Objects.equals(make, car.make) &&
+                Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(VIN, make, model);
+    }
+}
